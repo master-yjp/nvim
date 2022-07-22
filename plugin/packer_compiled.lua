47d95fd381d0832d02b6ee1ea984428e17c44421
MasterJ <masterj251016@gmail.com>
2022-07-22 22:14:09 +0800

    added lualine

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -124,6 +124,11 @@ _G.packer_plugins = {
     path = "/home/masterj/.local/share/nvim/site/pack/packer/start/friendly-snippets",
     url = "https://github.com/rafamadriz/friendly-snippets"
   },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/masterj/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
   ["markdown-preview.vim"] = {
     loaded = true,
     path = "/home/masterj/.local/share/nvim/site/pack/packer/start/markdown-preview.vim",
